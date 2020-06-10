d898e0a594250f2085d5e65a9fb83927be2bda88
Trevor Cheche <twitcherfrost@gmail.com>
2020-06-10 10:26:41 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,5 @@
-# Define display_board that accepts a board and prints
+def display_board(board)
+  board=[" "," "," "," "," "," "," "," "," "]
+end
+display_board(board)# Define display_board that accepts a board and prints
 # out the current state.
